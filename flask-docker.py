1a6d6cb88b77539351f82fcf8027c8dfdf378ec6
pree <2pnegi@gmail.com>
2019-03-30 18:37:32 +0000

    added project files

---
+++ flask-docker.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/name')
+def hello_world():
+    return 'Preeti Negi'
+
+@app.route('/')
+def hello_worldd():
+    return 'root path'
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
